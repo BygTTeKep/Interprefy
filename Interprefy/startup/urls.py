d9900854cb82e35f1cce390e874bda4676093555
Александр Айтуганоv <aytuganovv2@mail.ru>
2023-11-11 15:14:58 +0700

    add templates

--- Interprefy/startup/urls.py
+++ Interprefy/startup/urls.py
@@ -1,4 +1,14 @@
 from django.urls import path
+from .views import (
+    client_registration,
+    ajax_send_pin,
+)
+
+
+
+app_name = 'interprefy'
 
 urlpatterns = [
+    path('client/registration/', client_registration, name='client_registration'),
+    path('ajax_send_pin', ajax_send_pin, name='ajax_send_pin'),
 ]
