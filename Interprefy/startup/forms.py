28630229055e58cc8b4633f80bb2a8a1afe76db3
Александр Айтуганоv <aytuganovv2@mail.ru>
2023-11-11 14:54:06 +0700

    add forms

---
+++ Interprefy/startup/forms.py
@@ -0,0 +1,77 @@
+from typing import Any
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.forms import UserCreationForm
+from django.utils.text import slugify
+import re
+from .models import (
+    Reviews,
+    Clients,
+    Intermediares
+    
+)
+
+
+# def validate_phone_number(phonenumber):
+#     if re.search(r'^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$',phonenumber) == None:
+#         raise ValidationError(
+#             _("неверный формат номера телефона")
+#         )
+
+# def validate_fullname(fullname:str):
+#     if len(fullname.split()) != 3:
+#         raise ValidationError(
+#             _("%(value)s ")# <================================
+#         )
+    # И если есть другие знаки кроме букв
+
+class IntermediaresForm(forms.ModelForm):
+    Email = forms.EmailField(widget=forms.EmailInput(attrs={"type":"email"}))
+    Description = forms.CharField(widget=forms.Textarea(attrs={"rows":"5"}))
+    class Meta:
+        model = Intermediares
+        fields = ('Name', 'Email', 'Description')
+
+class ClientForm(UserCreationForm):
+    TelephoneNumber = forms.CharField(max_length=15,required=True,widget=forms.TextInput(attrs={'type': 'tel', 
+                                                                                                'class':"form-control", 
+                                                                                                "placeholder":"+7 (987)654-32-10",
+                                                                                                }))
+    FullName = forms.CharField(max_length=100, required=True,widget=forms.TextInput(attrs={'class':"form-control", 
+                                                                                           "placeholder":"Имя Фамилия Отчество", 
+                                                                                           "id":"FullName",
+                                                                                           "pattern": "",
+                                                                                           }))
+    pin = forms.IntegerField(required=True,min_value=10**5, max_value=10**6)
+    class Meta:
+        model = Clients
+        fields = 'username', 'FullName', 'TelephoneNumber', "password1", "password2"
+    
+
+
+    def __init__(self, *args, **kwargs):
+        # pattern для FullName и для TelephoneNumber
+        
+        super(ClientForm, self).__init__(*args, **kwargs)    
+        self.fields['TelephoneNumber'].widget.attrs['id'] = 'TelephoneNumber'
+        self.fields['TelephoneNumber'].widget.attrs['pattern'] = r'^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$'
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'   
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password1'].label = ''
+        self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        self.fields['password2'].label = ''
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
+
+
+class ReviewsForm(forms.ModelForm):
+    # Подумать над прикреплением картинки
+    class Meta:
+        model = Reviews
+        fields = ('Review','Image')
